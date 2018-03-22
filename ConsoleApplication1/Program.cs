b630faabd1d582de35201bbabf15cad584671cfa
m305-65\w401 <ddcc2100@gmail.com>
2018-03-22 16:26:37 +0800

    version01

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+            Random rand = new Random();
+
+            int r1 = rand.Next(0, 43);
+            int r2 = rand.Next(0, 43);
+            int r3 = rand.Next(0, 43);
+            int r4 = rand.Next(0, 43);
+            int r5 = rand.Next(0, 43);
+            int r6 = rand.Next(0, 43);
+
+            Console.Write("號碼{0:00},{1:00},{2:00},{3:00},{4:00},{5:00}",r1,r2,r3,r4,r5,r6);
+
+            Console.ReadLine();
+
+           
+
+
+        }
+    }
+}
